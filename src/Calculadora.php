6392be60cc46425f0b0282db83d1ebd59cba1fb8
CAM <zuniga.camilo@correounivalle.edu.co>
2026-06-03 15:21:33 -0500

    Add files via upload

---
+++ src/Calculadora.php
@@ -0,0 +1,21 @@
+<?php
+namespace App;
+
+class Calculadora {
+    public function sumar($a, $b) {
+        return $a + $b;
+    }
+
+    public function restar($a, $b) {
+        return $a - $b;
+    }
+
+    public function multiplicar($a, $b) {
+        return $a * $b;
+    }
+
+    public function dividir($a, $b) {
+        // TODO: Implementar en el taller
+        return 0; 
+    }
+}
